e40797b538e39cc3d85444fadf420e1de6b10c7e
livelycloud <qywangyj@gmail.com>
2021-09-07 22:24:36 +0800

    reorganized

---
+++ java_project2/src/myutils/Main.java
@@ -0,0 +1,21 @@
+package myutils;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+//        StringBuffer sb = new StringBuffer();
+//
+//
+//        sb.append("\n");
+//        pw.print(sb.toString());
+//        pw.flush();
+//        pw.close();
+
+
+
+    }
+}
